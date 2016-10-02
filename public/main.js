15ef967bc39fb5b1bae166e39caf2f2e447ee901
Nagappan <n4nagappan@gmail.com>
2016-10-02 17:34:00 +0530

    Adding radio buttons for wifi quality

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
+var template;
 var map;
 function initMap() {
+  var source   = $("#table-template").html();
+  template = Handlebars.compile(source);
+
   var current = {lat: 13.0849863, lng: 80.210193}
 
   var options = {
@@ -30,15 +34,20 @@ function placesCallback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
+      console.log( place );
       console.log( place.name );
-      $('#placeTable tbody').append('<tr><td> ' + place.name + ' </td></tr>');
+      var context = { "name" : place.name , "i" : i};
+      var html = template(context);
+
+      $('#placeTable tbody').append(html);
+
       // If the request succeeds, draw the place location on
       // the map as a marker, and register an event to handle a
       // click on the marker.
-      // var marker = new google.maps.Marker({
-      //   map: map,
-      //   position: place.geometry.location
-      // });
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
     }
   }
 };
